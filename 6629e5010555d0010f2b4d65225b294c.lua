5ced68d6ae989e8fbf5f941a5fc4b6e241c35d5a
facelessvssssssssssssssssss <notxagredd@gmail.com>
2024-11-06 21:52:50 +0700

    Update 6629e5010555d0010f2b4d65225b294c.lua

--- 6629e5010555d0010f2b4d65225b294c.lua
+++ 6629e5010555d0010f2b4d65225b294c.lua
@@ -5,5 +5,5 @@ gg.toast('‼️WAIT UNTIL PROCESS FINISHED‼️')
 gg.searchNumber("4;45;45;0::13", gg.TYPE_DWORD)
 gg.refineNumber("4", gg.TYPE_DWORD)
 Temp = gg.getResults(gg.getResultsCount())
-Temp1 = Temp(126)[126].address
+Temp1 = Temp(1)[126].address
 gg.addListItems(Temp)
